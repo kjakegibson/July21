f3ab2af2699e560754d931d478805e4f460d717f
kjakegibson <kjakegibson@hotmail.com>
2016-07-21 10:26:29 -0400

    initial commit 07212016

---
+++ src/com/dayfour/AnyOldClass.java
@@ -0,0 +1,10 @@
+package com.dayfour;
+
+public class AnyOldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//I don't understand classes and methods entirely and how they're built
+	}
+
+}
